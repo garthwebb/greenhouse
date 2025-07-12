9e1fd45181a2b209f63db7a1daee71f9f3612230
Garth Webb <garth.webb@gmail.com>
2025-07-12 07:57:20 -0700

    Added logging, and error checking for the HTTP connection.  Also addressed some memory leaks with the _doc object reuse.  Also resetting the HTTP connection on every monitor (happens once a minute) so that we don't get a stale connection that hangs and causes the watchdog to reset the esp32.

--- controller/code/lib/ExternalSettings/ExternalSettings.cpp
+++ controller/code/lib/ExternalSettings/ExternalSettings.cpp
@@ -1,19 +1,46 @@
 #include "ExternalSettings.h"
 #include <HTTPClient.h>
 
+#define HTTP_TIMEOUT 5000
+#define HTTP_CONNECT_TIMEOUT 3000
+
+extern Logger *LOGGER;
+
 HTTPClient HTTP;
 
-ExternalSettings::ExternalSettings(String host, uint16_t port, String path) {
-	HTTP.begin(host, port, path);
+ExternalSettings::ExternalSettings(String host, uint16_t port, String path) : _host(host), _port(port), _path(path) {
+	_reset_connection();
+}
+
+void ExternalSettings::_reset_connection() {
+	HTTP.end();
+	HTTP.begin(_host, _port, _path);
+	HTTP.setTimeout(HTTP_TIMEOUT);
+	HTTP.setConnectTimeout(HTTP_CONNECT_TIMEOUT);
 
 	const char* headerNames[] = {"Last-Modified"};
 	HTTP.collectHeaders(headerNames, sizeof(headerNames)/sizeof(headerNames[0]));
 }
 
 void ExternalSettings::monitor() {
+    _reset_connection();
+
 	Serial.println("Fetching external settings ...");
 
 	int httpCode = HTTP.GET();
+	
+	// Check for timeout or connection errors
+	if (httpCode == HTTPC_ERROR_CONNECTION_REFUSED || 
+	    httpCode == HTTPC_ERROR_SEND_HEADER_FAILED || 
+	    httpCode == HTTPC_ERROR_SEND_PAYLOAD_FAILED || 
+	    httpCode == HTTPC_ERROR_NOT_CONNECTED || 
+	    httpCode == HTTPC_ERROR_CONNECTION_LOST || 
+	    httpCode == HTTPC_ERROR_READ_TIMEOUT) {
+		LOGGER->log_error("HTTP request failed/timed out: " + String(httpCode));
+		Serial.printf("HTTP request failed/timed out: %d\n", httpCode);
+		return;
+	}
+
 	String lastModified = HTTP.header("Last-Modified");
 
 	if (_last_modified == lastModified) {
@@ -23,21 +50,18 @@ void ExternalSettings::monitor() {
 
 	_last_modified = lastModified;
 	Serial.printf("Response %d received, last modified: %s\n", httpCode, lastModified.c_str());
+	LOGGER->log("Updated external settings content detected, reloading ...");
 
 	String response = HTTP.getString();
 
-	// Deserialize the JSON document
+	// Clean up previous readings be for we deserialize the JSON document
+	_doc.clear();
   	DeserializationError error = deserializeJson(_doc, response);
 
 	// Test if parsing succeeds
 	if (error) {
-		Serial.print(F("deserializeJson() failed: "));
-		Serial.println(error.f_str());
+		LOGGER->log_error("deserializeJson() failed: " + String(error.f_str()));
+		Serial.println("deserializeJson() failed: " + String(error.f_str()));
 		return;
 	}
-
-	Serial.printf("Target temp: %d\n", get<int>("target_temp", 0));
-	Serial.printf("Target humidity: %d\n", get<int>("target_humidity", 0));
-	Serial.printf("Version: %f\n", get<float>("version", 0));
-	Serial.printf("FooBar: %s\n", get<String>("foo_bar", "42"));
 }
--- controller/code/lib/ExternalSettings/ExternalSettings.h
+++ controller/code/lib/ExternalSettings/ExternalSettings.h
@@ -6,14 +6,35 @@
 #include "Logger.h"
 #include "ArduinoJson.h"
 
+// Temp we want the greenhouse
+#define DEFAULT_TARGET_TEMP_F 70.0
+
+// Max and min define when windows & fan should always be open/on or always be closed/off
+#define DEFAULT_MAX_TEMP_F 80.0
+#define DEFAULT_MIN_TEMP_F 60.0
+
+#define DEFAULT_TEMP_SHORT_DETLA_S 5*60
+#define DEFAULT_TEMP_LONG_DETLA_S 60*60
+
+// Humidity we want the greenhouse
+#define DEFAULT_TARGET_HUMIDITY 60.0
+#define DEFAULT_MIST_ON_S 30
+#define DEFAULT_MIST_OFF_S 2*60
+
 class ExternalSettings {
     private:
 	String _last_modified = "";
 	JsonDocument _doc;
+	String _host;
+	uint16_t _port;
+	String _path;
+
+	void _reset_connection();
 
     public:
 
     ExternalSettings(String host, uint16_t port, String path);
+
 
 	void monitor();
 
@@ -30,6 +51,47 @@ class ExternalSettings {
 		} else {
 			return _doc[key].as<T>();
 		}
+	}
+
+	// Explicitly provide methods for all expected values
+	float get_target_temp_f() {
+		return get<float>("target_temp_f", DEFAULT_TARGET_TEMP_F);
+	}
+
+	float get_max_temp_f() {
+		return get<float>("max_temp_f", DEFAULT_MAX_TEMP_F);
+	}
+
+	float get_min_temp_f() {
+		return get<float>("min_temp_f", DEFAULT_MIN_TEMP_F);
+	}
+
+	int get_temp_long_delta_s() {
+		return get<int>("temp_long_detla_s", DEFAULT_TEMP_LONG_DETLA_S);
+	}
+
+	int get_temp_short_delta_s() {
+		return get<int>("temp_short_detla_s", DEFAULT_TEMP_SHORT_DETLA_S);
+	}
+
+	float get_target_humidity() {
+		return get<float>("target_humidity", DEFAULT_TARGET_HUMIDITY);
+	}
+
+	int get_mist_on_s() {
+		return get<int>("mist_on_s", DEFAULT_MIST_ON_S);
+	}
+
+	int get_mist_on_ms() {
+		return get_mist_on_s() * 1000;
+	}
+
+	int get_mist_off_s() {
+		return get<int>("mist_off_s", DEFAULT_MIST_OFF_S);
+	}
+
+	int get_mist_off_ms() {
+		return get_mist_off_s() * 1000;
 	}
 };
 
